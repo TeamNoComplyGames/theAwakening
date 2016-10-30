65208165117609a4af39cfa4f966f8371c306fb4
Aaron Turner <aaron@aaronthedev.com>
2016-10-29 19:00:51 -0700

    Added most objects, working good!

---
+++ Assets/MoveRightDelete.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class MoveRightDelete : MonoBehaviour {
+
+	//Our Move Speed
+	[Tooltip("How Much to add to our X position every frame eg 1 - 100")]
+	public float moveRateX = 3.0f;
+	private float xMoveTrue;
+	[Tooltip("How Much to add to our Y position every frame eg 1 - 100")]
+	public float moveRateY = 3.0f;
+	private float yMoveTrue;
+
+	//Our Delete Position
+	[Tooltip("Our Max X position before deleting. Uses absolute value to decide the intended direction")]
+	public int deleteX = 3;
+	[Tooltip("Our Max X position before deleting. Uses absolute value to decide the intended direction")]
+	public int deleteY = 3;
+
+	// Use this for initialization
+	void Start () {
+		//Get our true values
+		xMoveTrue = (float) moveRateX / 100.0f;
+		yMoveTrue = (float) moveRateY / 100.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Check if we need to delete the object
+		if(Math.Abs(gameObject.transform.position.x) > deleteX) Destroy(gameObject);
+		if(Math.Abs(gameObject.transform.position.y) > deleteY) Destroy(gameObject);
+
+		//Else, move the object
+		Vector2 newPos = new Vector2(gameObject.transform.position.x + xMoveTrue, gameObject.transform.position.y + yMoveTrue);
+		gameObject.transform.position = newPos;
+	}
+}
---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+
+	//Array of Prefabs to be spawned
+	[Tooltip("An Array of prefabs of objects")]
+	public GameObject[] objects;
+
+	//Our spawn rate
+	[Tooltip("Our spawn rate per second 1 through e.g 300")]
+	public int spawnRate = 5;
+	private int trueSpawnRate = 0;
+	private int currentSpawn = 0;
+
+	// Use this for initialization
+	void Start () {
+		//Set our true spawn rate (60 frames per second), 60 was a bit too long, so shortened it a little bit
+		trueSpawnRate = 5 * 45;
+		currentSpawn = trueSpawnRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentSpawn <= 0 ) {
+			//Spawn an enemy!
+			if(objects.Length > 0)StartCoroutine("SpawnEnemy");
+			currentSpawn = trueSpawnRate;
+		} else currentSpawn--;
+	}
+
+	//Function to spawn and object
+	IEnumerator SpawnEnemy() {
+
+		Vector2 spawnPos = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+
+		//Get a random number from the array
+		int ranIndex = (int) Math.Floor(UnityEngine.Random.value * objects.Length);
+
+		//Spawn our enemy
+		Instantiate(objects[ranIndex], spawnPos, Quaternion.identity);
+
+		yield return null;
+	}
+}
